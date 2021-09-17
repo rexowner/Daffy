d63ce8d1cf3cfbb9a1a207111fa0440029f8c2f1
Les Poltrack <lapwack@gmail.com>
2021-09-17 13:05:04 -0700

    Initial Commit

---
+++ Shared/DaffyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaffyApp.swift
+//  Shared
+//
+//  Created by Les Poltrack on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct DaffyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
